a142f9148b9c022048b408fe9c09cca6bff76b67
Ricky Hu <lakeseafly@icloud.com>
2021-12-06 14:27:42 +0800

    Update 1.SV_analysis.sh

--- 1. SV_analysis/1.SV_analysis.sh
+++ 1. SV_analysis/1.SV_analysis.sh
@@ -7,3 +7,13 @@
 ### Running Syri to detect the SV between EC-S1 and EC-N1 genome
 
 python3.5 syri -c EC-S1_VS_EC-N1.filter.coords -d EC-S1_VS_EC-N1.filter.delta -r EC-S1_pseudomolecules_V1_1kb_ref.fasta -q EC-N1_pseudomolecules_V1_1kb_ref.fasta--nosnp --prefix EC-S1_VS_EC-N1.syri --lf EC-S1_VS_EC-N1_ref.log
+
+#### Genotyped the SVs identifed in Syri in 22 wild barley accessions using Paragraph
+for i in `cat accession.list`; do 
+multigrmpy.py -i Syri_SVs.vcf -m ${i}.sample.txt -r fixorder_Damai_S_pseudomolecules_V1.fasta -M 284 -o ${i}_genotype -t 24;
+done
+
+#### Merge all the genotyped vcf files into one population-wide SVs VCF file
+
+ls -1 *_genotype.vcf > all.SVs.list
+SURVIVOR merge all.SVs.list 1000 2 1 1 0 50 final.merge.vcf
