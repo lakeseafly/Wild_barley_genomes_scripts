20a53ce629779ed75f41af393c59432fd7fbd579
Ricky Hu <lakeseafly@icloud.com>
2021-12-06 14:58:35 +0800

    Delete heterozygote_deficit_cal directory

--- heterozygote_deficit_cal/run_heterozygote_deficit.sh
+++
@@ -1,6 +0,0 @@
-### using the plink to calculate the Hobs, Hexp and F value
-plink --vcf SNPs.vcf --hardy 
-
-### using the windowscanr to estimate the F value by 100kb windows
-
-Rscript Hobs_sliding.r SNP_plink.hwe 100000 10000
